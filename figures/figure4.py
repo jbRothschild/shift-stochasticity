03b3217aaa9df013beb1f73a3c16d8579281e170
jbrothschild <jerben.rothschild@gmail.com>
2019-12-12 12:56:25 -0500

    The only problem we have right now is that there is a mathematical function in shiftStochasticity.py which is problematic. We have that (negative number)**(fractional power). This cannot be, so we need to find where the error is.

--- figures/figure4.py
+++ figures/figure4.py
@@ -10,7 +10,6 @@ plt.style.use('parameters.mplstyle')  # particularIMporting
 
 from matplotlib.ticker import LogLocator #log axes
 
-
 maximum = ss.maxPop(cap[K], stochasticity[sto], variability[var])
 population = np.linspace(1,maximum,maximum)
 
@@ -27,10 +26,10 @@ PDF.append(ss.statDistributionAlgo(population, stochasticity[sto], cap[K], varia
 # small n approx
 #PDF.append(ss.pdf_smalln_recursive_list(population, stochasticity[sto], cap[K], variability[var]))
 
-figname = 'Fig4' + '_q' + str(round(stochasticity[sto],3)) + '_d' + str(round(variability[var],3))
+figname = 'Fig4_1' + '_q' + str(round(stochasticity[sto],3)) + '_d' + str(round(variability[var],3))
 plt.figure()
 ax = plt.gca()
-
+print(PDF[0])
 #for i in range(0,len(PDF)):
 for i in [0,1,2,3]:
     ax.plot(population, PDF[i], color=colors_techniques[i], label = techniques[i], linestyle=lines[i], lw=3)
@@ -39,10 +38,11 @@ for i in [0,1,2,3]:
 ax.set_xlabel("Population")
 ax.set_ylabel("Probability distribution function")
 ax.set_yscale("log")
-ax.set_ylim(10**(-20), 10**(0))
+ax.set_ylim(10**(-50), 10**(0))
+ax.set_xlim(0, 250)
 ax.get_yaxis().set_major_locator(LogLocator(numticks=5))
 ax.minorticks_off()
-ax.set_xlim(1, 200)
+#ax.set_xlim(1, np.max(population))
 ax.legend(loc = 'lower center')
 
 plt.savefig(DIR_OUTPUT + os.sep + figname + '.pdf', transparent=True)
